44a34ff42bb1b6f358ab23f067c0ed9dcf726ddc
wissamsaikali <wissam.saikali@elementn.com>
2018-08-30 16:33:34 +0300

    Update GridFTSearch.js

--- ApstrataSDK/apstrata/horizon/GridFTSearch.js
+++ ApstrataSDK/apstrata/horizon/GridFTSearch.js
@@ -1,57 +1,31 @@
 dojo.provide("apstrata.horizon.GridFTSearch")
 
-
 dojo.require("dijit.form.Form")
 dojo.require("dijit.form.Button");
 dojo.require("dijit.form.ValidationTextBox");
 dojo.require("dojox.dtl.Inline");
 
-dojo.declare("apstrata.horizon.GridFTSearch",
-	[dijit._Widget, dojox.dtl._Templated],
-	{
-		templatePath: dojo.moduleUrl("apstrata.horizon", "templates/GridFTSearch.html"),
-		widgetsInTemplate: true,
-
-		titleLabel: "Title:",
-		searchActionLabel: "Search",
-
-		constructor: function (args) {
-			this.notAdvancedSearch = !args.advancedSearch;
-			this.basicMode = (!args.advancedSearchOpt.defaultAdvancedMode) && args.advancedSearch;
-			this.advancedMode = (args.advancedSearchOpt.defaultAdvancedMode && args.advancedSearch);
-		},
-		postCreate: function () {
-			this.update();
-		},
-
-		_search: function () {
-			this.search({
-				type: "normal",
-				search: this.frmSearchNormal.get('value').search
-			})
-		},
-		_search_basic: function () {
-			var data = {
-				type: "basic"
-			};
-			for(item of this.advancedSearchOpt.basicColumns){
-				data[item.code]=this.frmSearchBasic.get('value')[item.code];
-			}
-			this.search(data)
-			var query = this.query;
-			query.set("value","");
-		},
-
-		enable_advanced: function () {
-			this.basicMode = false;
-			this.advancedMode = true;
-			this.update();
-		},
-		enable_basic: function () {
-			this.basicMode = true;
-			this.advancedMode = false;
-			this.update();
-		},
-
-		search: function (attr) { }
-	})
+dojo.declare("apstrata.horizon.GridFTSearch", 
+[dijit._Widget, dojox.dtl._Templated], 
+{
+	templatePath: dojo.moduleUrl("apstrata.horizon", "templates/GridFTSearch.html"),
+	widgetsInTemplate: true,
+	
+	titleLabel: "Title:",
+	searchActionLabel: "Search",
+	constructor: function (args) {
+		this.notAdvancedSearch = !args.advancedSearch;
+		this.basicMode = (!args.advancedSearchOpt.defaultAdvancedMode) && args.advancedSearch;
+		this.advancedMode = (args.advancedSearchOpt.defaultAdvancedMode && args.advancedSearch);
+	},
+	postCreate: function () {
+		this.update();
+	},
+	_search: function() {
+		this.search({
+			search: this.frmSearch.get('value').search
+		})
+	},
+	
+	search: function(attr) {}
+});
