92a1bbcf83d0694c63f657e74b9a6f280c50f1c2
wissamsaikali <wissam.saikali@elementn.com>
2018-08-30 16:39:21 +0300

    Update GridFTSearch.js

--- ApstrataSDK/apstrata/horizon/GridFTSearch.js
+++ ApstrataSDK/apstrata/horizon/GridFTSearch.js
@@ -29,16 +29,6 @@ dojo.declare("apstrata.horizon.GridFTSearch",
 		})
 	},
 	
-	_search_basic: function () {
-		var data = {
-			type: "basic"
-		};
-		for(item of this.advancedSearchOpt.basicColumns){
-			data[item.code]=this.frmSearchBasic.get('value')[item.code];
-		}
-		this.search(data)
-		var query = this.query;
-		query.set("value","");
-	},
+
 	search: function(attr) {}
 });
